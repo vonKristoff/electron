5417b61223d3f0746dd960683c990b13c5b04863
vonKristoff <mrjcnicolas@gmail.com>
2016-12-15 10:35:44 +0000

    update config

--- config.js
+++ config.js
@@ -5,21 +5,25 @@ const path = require('path')
 const pkg = require('./package.json')
 
 let config = {
-  name: pkg.name,
-  port: 9080,
-  env: env,
-  // electron-packager options
-  building: {
-    'app-version': pkg.version,
-    arch: 'x64',
-    asar: true,
-    dir: path.join(__dirname, 'app'),
-    icon: path.join(__dirname, 'app/icons/icon'),
-    ignore: /node_modules|src|main.ejs|icons/,
-    out: path.join(__dirname, 'builds'),
-    overwrite: true,
-    platform: process.env.PLATFORM_TARGET || 'all'
-  }
+    name: pkg.name,
+    port: 9080,
+    env: env,
+    // electron-packager options
+    building: {
+        'app-version': pkg.version,
+        arch: 'x64',
+        asar: true,
+        dir: path.join(__dirname, 'app'),
+        icon: path.join(__dirname, 'app/icons/icon'),
+        ignore: /node_modules|src|main.ejs|icons/,
+        out: path.join(__dirname, 'builds'),
+        overwrite: true,
+        platform: process.env.PLATFORM_TARGET || 'all'
+    },
+     app: {
+        width: 1920,
+        height: 1080
+    }
 }
 
 config.building.name = config.name
